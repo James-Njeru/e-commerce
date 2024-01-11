3e3d5379bc23b3602d28dbcd65cec006fdb15ce6
Jimmy37329019 <jamesnjeru397@gmail.com>
2024-01-11 18:49:51 +0300

    second commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,107 @@
+import Acc from "./acc.jpg";
+import AddCart from "./add-cart.svg";
+import Arrow from "./arrow.svg";
+import Blog from "./blog-1.jpg";
+import Brand1 from "./brand-01.png";
+import Brand2 from "./brand-02.png";
+import Brand3 from "./brand-03.png";
+import Brand4 from "./brand-04.png";
+import Brand5 from "./brand-05.png";
+import Brand6 from "./brand-06.png";
+import Brand7 from "./brand-07.png";
+import Brand8 from "./brand-08.png";
+import Camera from "./camera.jpg";
+import Cart from "./cart.svg";
+import Catbanner1 from "./catbanner-01.jpg";
+import Catbanner2 from "./catbanner-02.jpg";
+import Catbanner3 from "./catbanner-03.jpg";
+import Catbanner4 from "./catbanner-04.jpg";
+import Compare from "./compare.svg";
+import Cross from "./cross.svg";
+import Del from "./del.svg";
+import EmptyCart from "./empty-cart.jpg";
+import Gr from "./gr.svg";
+import Gr2 from "./gr2.svg";
+import Gr3 from "./gr3.svg";
+import Gr4 from "./gr4.svg";
+import Headphone from "./headphone.jpg";
+import Homeapp from "./homeapp.jpg";
+import Imgzoom from "./img-zoom.svg";
+import Laptop from "./laptop.jpg";
+import Mainbanner from "./main-banner.jpg";
+import Mainbanner1 from "./main-banner-1.jpg";
+import Menu from "./menu.svg";
+import Newsletter from "./newsletter.png";
+import Prodcompare from "./prodcompare.svg";
+import Service from "./service.png";
+import Service2 from "./service-02.png";
+import Service3 from "./service-03.png";
+import Service4 from "./service-04.png";
+import Service5 from "./service-05.png";
+import Speaker from "./speaker.jpg";
+import Tab from "./tab.jpg";
+import Tab1 from "./tab1.jpg";
+import Tab2 from "./tab2.jpg";
+import Tab3 from "./tab3.jpg";
+import Tv from "./tv.jpg";
+import User from "./user.svg";
+import View from "./view.svg";
+import Watch from "./watch.jpg";
+import Wishblack from "./wish-black.svg";
+import Wish from "./wish.svg";
+import Wishlist from "./wishlist.svg";
+
+export {
+  Acc,
+  AddCart,
+  Arrow,
+  Blog,
+  Brand1,
+  Brand2,
+  Brand3,
+  Brand4,
+  Brand5,
+  Brand6,
+  Brand7,
+  Brand8,
+  Camera,
+  Cart,
+  Catbanner1,
+  Catbanner2,
+  Catbanner3,
+  Catbanner4,
+  Compare,
+  Cross,
+  Del,
+  EmptyCart,
+  Gr,
+  Gr2,
+  Gr3,
+  Gr4,
+  Headphone,
+  Homeapp,
+  Imgzoom,
+  Laptop,
+  Mainbanner,
+  Mainbanner1,
+  Menu,
+  Newsletter,
+  Prodcompare,
+  Service,
+  Service2,
+  Service3,
+  Service4,
+  Service5,
+  Speaker,
+  Tab,
+  Tab1,
+  Tab2,
+  Tab3,
+  Tv,
+  User,
+  View,
+  Watch,
+  Wish,
+  Wishblack,
+  Wishlist,
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
+import { Compare, Wishlist, User, Cart } from "../assets/images";
 
 function Header() {
   return (
@@ -47,7 +48,43 @@ function Header() {
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={Compare} alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={Wishlist} alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={User} alt="user" />
+                    <p className="mb-0">
+                      Log in <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={Cart} alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
