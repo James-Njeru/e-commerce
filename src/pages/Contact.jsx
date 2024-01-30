7d6f11266890a3ab972ab064223b575a2be8556d
Jimmy37329019 <jamesnjeru397@gmail.com>
2024-01-30 13:37:57 +0300

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import BreadCrumb from "../components/BreadCrumb";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26834.53133034207!2d36.882635329632535!3d-1.1766918901028935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ske!4v1706605963794!5m2!1sen!2ske"
+                height="450"
+                className="border-0 w-100"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols={30}
+                        rows={4}
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5" />
+                        <address className="mb-0">
+                          Bazaar Plaza Moi Ave, Nairobi
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+123 456 7890">+123 456 7890</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:jamesnjeru397@gmail.com">
+                          jamesnjeru397@gmail.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM - 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
